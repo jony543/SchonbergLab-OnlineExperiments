8d1bdabaa3587b6384c1f805f49d3c09e2979ab7
Jonathan <ataria@windowsive.com>
2021-05-19 14:51:13 +0300

    implemented session upsertMany

--- server/controllers/session.controller.js
+++ server/controllers/session.controller.js
@@ -8,10 +8,10 @@ module.exports = {
 async function getBySubdId(subId, fields, fromDate) {
 	if (isNaN(subId)) {
 		logger.warn('getBySubId called with illegal value: ' + subId);
-    	return []; 
+    return [];
 	}
 
-	var query = Session.find({ subId: subId });	
+	var query = Session.find({ subId: subId });
 	if (fromDate) {
 		query.where('updated_at').gt(fromDate);
 	}
@@ -23,5 +23,38 @@ async function getBySubdId(subId, fields, fromDate) {
 	return await query.exec()
 		.catch(function (e) {
 			logger.error('error in getBySubId. subId=' + subId, e);
+	});
+}
+
+async function upsertMany(subId, docs) {
+  if (isNaN(subId)) {
+		logger.warn('upsertMany called with illegal value: ' + subId);
+    return { 'error': 'illegal subId' };
+	}
+
+  var bulkOps = [];
+  docs.forEach((doc) => {
+    doc.patched_at = Date.now();
+    if (!!doc._id) {
+      doc.patchStatus = 'patched';
+      bulkOps.push({
+        'updateOne': {
+          'filter': { '_id': doc._id },
+          'update': doc,
+        }
+      });
+    } else {
+      doc.patchStatus = 'inserted';
+      bulkOps.push({
+        'insertOne': {
+          'document': doc,
+        }
+      });
+    }
+  });
+
+  return await Session.bulkWrite(bulkOps)
+		.catch(function (e) {
+      logger.error('error in upsertMany. subId=' + subId, e);
 	});
 }
--- server/routes/session.route.js
+++ server/routes/session.route.js
@@ -5,13 +5,18 @@ const sessionCtrl = require('../controllers/session.controller');
 const router = express.Router();
 module.exports = router;
 
-// example call: GET http://localhost:4040/app/api/session/list?subId=5
-router.route('/list').get(asyncHandler(list));
+router.route('/list').get(asyncHandler(list)); // example call: GET http://localhost:4040/app/api/session/list?subId=5
+router.route('/').post(asyncHandler(post));
 
 async function list(req, res) {
 	const fromDate = (req.query.from) ? new Date(req.query.from) : undefined;
 	const fields = (req.query.fields) ? req.query.fields.split(',') : undefined;
 
 	let results = await sessionCtrl.getBySubdId(req.query.subId, fields, fromDate);
+	res.json(results);
+}
+
+async function post(req, res) {
+	let results = await sessionCtrl.upsertMany(req.query.subId, req.body);
 	res.json(results);
 }
